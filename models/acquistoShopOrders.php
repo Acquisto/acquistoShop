e0c232e0f376d7784946d2f4c09985c7a5d48d2c
root <brandhoff@contao-acquisto.de>
2013-10-28 22:45:32 +0100

    Fixed #26

--- models/acquistoShopOrders.php
+++ models/acquistoShopOrders.php
@@ -42,7 +42,7 @@ class acquistoShopOrders extends \Controller {
 
         $objOrder = $objOrder->row();
         
-        $objOrder['items']             = $this->getOrderItems($orderId);
+        $objOrder['items']             = $this->getOrderItems($orderId, true);
         $objOrder['customData']        = (object) unserialize($objOrder['customData']);
         $objOrder['customFields']      = (object) unserialize($objOrder['customFields']);
         $objOrder['customerData']      = (object) unserialize($objOrder['customerData']);
@@ -57,7 +57,7 @@ class acquistoShopOrders extends \Controller {
         return (object) $objOrder;        
     }
     
-    public function getOrderItems($orderId) 
+    public function getOrderItems($orderId, $round = false) 
     {
         $objOrder    = $this->Database->prepare("SELECT tstamp FROM tl_shop_orders WHERE id=?")->execute($orderId);
         $objOrderItems = $this->Database->prepare("SELECT * FROM tl_shop_orders_items WHERE pid=?")->execute($orderId);
@@ -67,7 +67,12 @@ class acquistoShopOrders extends \Controller {
             $objSteuer = $this->Database->prepare("SELECT * FROM tl_shop_steuersaetze WHERE pid=? && tstamp<? ORDER  BY tstamp ASC")->limit(1)->execute($objItem->steuersatz_id, $objOrder->tstamp);                       
 
             $objItem->preis = round($objItem->preis * ($objSteuer->satz / 100 + 1), 2) / ($objSteuer->satz / 100 + 1);
-            $objItem->summe = sprintf("%01.2f", $objItem->preis * $objItem->menge);  
+            $objItem->summe = sprintf("%01.2f", $objItem->preis * $objItem->menge);
+            
+            if($round)
+            {
+                $objItem->preis = sprintf("%01.2f", $objItem->preis);            
+            }  
             
             $arrItems[] = $objItem; 
         }
